89b2a2532d3dd7572dff21d4040981e59d7df860
maxi <maxisantomil90@gmail.com>
2021-08-28 13:18:14 -0300

    pintar una region rectangular de un color, adicionalmente forme una piramide de rectangulos

---
+++ TP1/js/ejercicio2.js
@@ -0,0 +1,13 @@
+var ctx = document.getElementById("canvas").getContext("2d");
+ctx.fillStyle = "#002550"; //color de fondo
+ctx.fillRect(250, 25, 50, 50); //dibuja un rectangulo los dos primeros valores corresponden a eje x e y
+ctx.fillStyle = "#781510";
+ctx.fillRect(275, 75, 50, 50);
+ctx.fillStyle = "#332233";
+ctx.fillRect(225, 75, 50, 50);
+ctx.fillStyle = "#33FCFF";
+ctx.fillRect(200, 125, 50, 50);
+ctx.fillStyle = "#624789";
+ctx.fillRect(250, 125, 50, 50);
+ctx.fillStyle = "#F9FF33";
+ctx.fillRect(300, 125, 50, 50);
