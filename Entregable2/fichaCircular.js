fd11b31d77daae92dad139e536ed017bd5e7ede5
maxi <maxisantomil90@gmail.com>
2021-09-24 19:34:31 -0300

    se agrega imagen a la ficha, primer intento

--- Entregable2/fichaCircular.js
+++ Entregable2/fichaCircular.js
@@ -9,11 +9,22 @@ class fichaCircular extends Ficha {
         super.draw();
         this.context.beginPath();
         this.context.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
+        let image1 = new Image(); //iniciar ruta
+        image1.src = 'img/poker.png'; //nuevo objeto imagen
+        image1.onload = () => { //la imagen debe cargarse
+            let relleno = this.context.createPattern(image1, "repeat"); //método createPattern
+            this.context.fillStyle = relleno; //imagen como relleno
+            this.context.fill();
+        }
         this.context.fill();
         this.context.closePath();
     }
 
     getRadius() {
         return this.radius;
+    }
+
+    fillImage() {
+
     }
 }
--- Entregable2/index.js
+++ Entregable2/index.js
@@ -4,10 +4,10 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 for (let i = 0; i < 10; i++) {
     let fichas_j1 = new fichaCircular(50, 50 + i * 50, 'red', ctx);
-    ficha_p1.draw();
+    fichas_j1.draw();
 }
 
 for (let i = 0; i < 10; i++) {
     let fichas_j2 = new fichaCircular(850, 50 + i * 50, 'blue', ctx);
-    ficha_p2.draw();
+    fichas_j2.draw();
 }
