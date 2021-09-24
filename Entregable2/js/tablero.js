c954c9efc581b7911800b8c32958f8478d6b288b
JoseProz <joseproz1010@gmail.com>
2021-09-24 20:23:06 -0300

    se crea clase tablero

---
+++ Entregable2/js/tablero.js
@@ -0,0 +1,9 @@
+class Tablero{
+constructor(){
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    let width = canvas.width;
+    let heigth = canvas.heigth;
+    
+}
+}
