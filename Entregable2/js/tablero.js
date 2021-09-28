e38de242b32a072908c701d0071475cb5134db8e
JoseProz <joseproz1010@gmail.com>
2021-09-28 16:57:30 -0300

    se agrega tablero

--- Entregable2/js/tablero.js
+++ Entregable2/js/tablero.js
@@ -1,9 +1,24 @@
 class Tablero{
-constructor(){
-    let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
-    let width = canvas.width;
-    let heigth = canvas.heigth;
+constructor(posX, posY, context, width, height){
+    this.posX=posX;
+    this.posY=posY;
+    this.fill=fill;
+    this.context=context;
+    this.widthT=width;
+    this.heightT=height;
+
+    let image1 = new Image();
+    image1.src = "img/1.png";
     
+    image1.onload = function() {
+        myDrawImageMethod(this);
+    }
+    function myDrawImageMethod(image) {
+        ctx.drawImage(image, 150, 150);
+    }
 }
+
+
+
+
 }
